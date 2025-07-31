a8f516cbdc087d4391de4b2d106dacaaa73633fe
Rabby-152 <mdrabby30824@gmail.com>
2025-07-31 08:26:01 +0600

    Add files via upload

---
+++ package.py
@@ -0,0 +1,30 @@
+import os
+os.system("clear")
+
+print("""\033[1m\033[1;32m 
+  ___    _   ___ _____   __
+ | _ \  /_\ | _ ) _ ) \ / /
+ |   / / _ \| _ \ _ \\ V / 
+ |_|_\/_/ \_\___/___/ |_|  
+_______________________________________________________      
+\033[0m[\033[1;32m.\033[0m] \033[1mOwer        : \033[1mRabby
+[\033[1;32m.\033[0m] \033[1mGithub       : \033[1mNothing
+[\033[1;32m.\033[0m] \033[1mFacebook    :\033[1m Md Rabby
+\033[1;32m_______________________________________________________
+\033[0m[\033[1;32m!\033[0m] \033[1mFOLLOW MY FACEBOOK : Brooo...!!
+\033[1;32m______________________________________________________\033[0m
+""")                    
+                                      
+os.system("pkg update")
+os.system("pkg upgrade")
+os.system("pkg install python")
+os.system("pip install requests")
+os.system("pip install mechanize")
+os.system("pip install bs4")
+os.system("pkg install git")
+os.system("pip install rich")
+os.system("pkg install espeak")
+os.system("pip install pycurl")
+os.system("pip install httpx")
+os.system("termux-setup-storage")
+print("All Package Installed Successfully!")
